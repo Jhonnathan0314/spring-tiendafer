36ab532813c9dea2ccb33aad319f9c52accb8d80
JhonnathanIAS <jonatan.franco@ias.com.co>
2023-03-20 11:20:15 -0500

    Cors solved

---
+++ src/main/java/com/spring/tiendafer/WebMvcConfig.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.spring.tiendafer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author JONATAN FRANCO
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+	
+}
